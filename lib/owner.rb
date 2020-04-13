0b65fcd542f05dbbe44b8d5958784484e5087f06
rosa lee <rosalee0116@gmail.com>
2020-04-13 02:24:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
   # code goes here
+  attr_reader :name
+
+  @@all = [ ]
+
+  @@all_pets = [ ]
+
+  def initialize(name)
+    @name = name
+    @cats = []
+    @dogs = []
+    @@all << self
+  end
+
+   def self.all 
+     @@all 
+   end
+
+   def self.count
+     @@all.length
+   end
+
+   def self.reset_all
+     @all.clear
+   end
+
+   def species(species = "human")
+     species
+   end
+
+   def say_species
+     "I am a #{self.species}."
+   end
+   
 end
