ab6b57b0d51fb6e4d48e2eacde916ddb6a595fa6
rosa lee <rosalee0116@gmail.com>
2020-04-13 02:27:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,5 +32,20 @@ class Owner
    def say_species
      "I am a #{self.species}."
    end
-   
+
+   def cats 
+     Cat.all.select{|item| item.owner == self}
+   end
+
+   def dogs 
+     Dog.all.select{|item| item.owner == self}
+   end
+
+   def buy_cat(name)
+     @cats << Cat.new(name, self)
+   end
+
+   def buy_dog(name)
+     @dogs << Dog.new(name, self)
+   end
 end
